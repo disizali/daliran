88f9bf8a4e419c25e6a19c692e1c820556f47380
Ali <a.hassssani@gmail.com>
2019-10-04 19:15:07 +0330

    moving sky

--- components/Splash.js
+++ components/Splash.js
@@ -1,30 +1,25 @@
 import React from "react";
 import { Container } from "reactstrap";
-import { Player, ControlBar, BigPlayButton } from "video-react";
 import "../styles/index.scss";
 
 export default class Splash extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
-    this.player.play();
-  }
+
   render() {
     return (
       <section className="splash-container">
-        <Player
-          autoPlay
-          loop={true}
-          id="player"
-          ref={player => {
-            this.player = player;
-          }}
-        >
-          <source src="/static/videos/cover.mp4" />
-          <BigPlayButton position="center" className="d-none" />
-          <ControlBar disableDefaultControls className="d-none" />
-        </Player>
+        <img
+          src="/static/images/cover-frame.png"
+          className="bg-transparent splash-cover-frame"
+          width="100%"
+        />
+        <img
+          src="/static/images/cover-sky.png"
+          className="position-absolute splash-cover-sky"
+          width="100%"
+        />
         <Container>
           <div className="slogan">
             <span className="slogan-top">
